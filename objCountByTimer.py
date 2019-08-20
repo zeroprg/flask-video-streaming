a85732a671d639a58f84477eabef99b52a0c3eb5
Vitaly Arkhipov <zeroprg@yahoo.com>
2019-08-20 12:28:47 -0600

    unit test passed

---
+++ objCountByTimer.py
@@ -0,0 +1,78 @@
+'''
+Class which populated quantity of hashcodes by time scale :  [1 minute, 5 minutes, ..., 60 minutes  ]
+to get how many unique hashcode were populated call self.get(i) where i index of time scale
+self.get(0): return how many hash codes populated for last minutes
+self.get(1): return how many hash codes populated for last 5 minutes
+...
+self.get(n): return how many hash codes populated for last 60 minutes
+
+self.add(hash): To add hash code to the queue
+override or change self.equals(hash1, hash2) method
+
+'''
+from collections import deque
+
+import threading
+import time
+
+class ObjCountByTimer:
+    ''' This class count different objects ( by hashcode) by using timer  '''
+    def __init__(self, timestep=1, timescope=65, time_scale=(1,5,30,60)):
+        self.store = deque()
+        self.timescope = timescope # in seconds
+        self.timestep = timestep # in seconds
+        self.interval = 0
+        self.time_scale = time_scale
+        self.counted = []
+        for i in range(len(time_scale)): self.counted.append(0)
+        self.t1 = threading.Timer(timestep, self.scheduler)
+        self.t1.start()
+      
+    def right_shift(self, elem, index):
+        #print("self.time_scale: {}".format(self.time_scale) )
+        for _i in range(len(self.time_scale)):
+            #print("t:{} ,elem:{}".format(self.time_scale[_i],elem))
+            if self.time_scale[_i] > elem[0]:
+                #shift all elements on 1 sec to right
+                self.counted[_i] = index + 1 
+                #print("self.sliced_result[{}] {}".format(_i,self.counted[_i]))
+                return
+
+    def scheduler(self):
+        ''' This scheduler called by timer every self.timescope/10 sec '''
+        #print("Scheduler called queue: {}".format(list(self.store)))
+        ls = list(self.store)
+        #print("ls[0]: {}".format(ls[0]) )
+        #print("ls[0]: {} len(ls): {} ".format(ls[0], len(ls)) )
+        for index in range(len(ls)):
+            #print("elem:{}".format(ls[index]))
+            ls[index][0] += self.timestep # increase in seconds
+            #slice 
+            self.right_shift(ls[index], index)
+            # when we reach last second of timescope
+            if ls[index][0] > self.timescope: 
+                #remove the last element from the queue  which is out of time scope
+                self.store.pop()
+                break
+        self.t2 = threading.Timer(self.timestep, self.scheduler)
+        self.t2.start()
+    def stop(self):
+        self.t1.cancel()    
+        self.t2.cancel()
+        
+    #implements equals
+    def equals(self,hash1, hash2):
+        return hash1==hash2
+        
+    def add(self,hashcode):
+        ''' Add a new object to queue , only if there is no "equal" objects ''' 
+        n=0
+        for elem in list(self.store):
+            if self.equals(hashcode, elem):
+                break
+            n+=1
+        #print('self.store.qsize({}), n={}'.format(len(self.store), n))    
+        if len(self.store) == n:
+            self.store.append([0,hashcode])
+
+        
--- test_ObjCountByTimer.py
+++ test_ObjCountByTimer.py
@@ -1,18 +1,35 @@
 import unittest
-import timer
-from ObjCountByTimer import ObjCountByTimer
+import time
+from objCountByTimer import ObjCountByTimer
 
 class TestObjCountByTimer(unittest.TestCase):
 
     def test_description(self):
 
-        obj = ObjCountByTimer(10, [1,2,5,10])
+        obj = ObjCountByTimer(1,65, (5,10,60))
         obj.add("Privet Lunatikam!!!")
         obj.add("Privet!!!")
         obj.add("Pri!!!")
-        time.sleep(80)
-        self.assertEqual(obj.get(1), 3)
-        print('obj.get(0):{} '.format(obj.get(1)))
+        time.sleep(5)
+        # subtest 1
+        print('obj.store: {} obj.sliced_result {}'.format(obj.store, obj.counted))
+        print('How many different obj.counted[0] after 5 sec: {}'.format(obj.counted[0]))
+        self.assertEqual(obj.counted[0], 3)
+        time.sleep(5.0)
+        
+        # subtest 2
+        print('obj.store: {} obj.sliced_result {}'.format(obj.store, obj.counted))
+        print('How many different obj.counted[1] after 10  sec: {}'.format(obj.counted[1]))
+        self.assertEqual(obj.counted[1], 3)
+        time.sleep(90.0)
+
+        # subtest 3
+        print('obj.store: {} obj.sliced_result {}'.format(obj.store, obj.counted))
+        print('How many different obj.counted[2] after >60 (100sec)  sec: {}'.format(obj.counted[2]))
+        self.assertEqual(obj.counted[2], 3)
+        
+        obj.stop()
+
 
 test = TestObjCountByTimer()
 test.test_description
