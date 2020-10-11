20a63d264a4783907d15ccb62cab9b732c2c4748
zeroprg <zeroprg@yahoo.com>
2020-10-11 20:37:55 +0000

    tuned config

--- classifier_yolo.py
+++ classifier_yolo.py
@@ -27,7 +27,7 @@ DIMENSION_X = 416
 DIMENSION_Y = 416
 piCameraResolution = (640, 480)  # (1024,768) #(640,480)  #(1920,1080) #(1080,720) # (1296,972)
 piCameraRate = 16
-NUMBER_OF_THREADS = 1
+NUMBER_OF_THREADS = 3
 BOX_EXTENDER = 60
 
 class Detection:
