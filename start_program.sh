d9c5cd582ba14393c8873879f23c422180081f37
zeroprg <zeroprg@yahoo.com>
2020-10-14 01:01:31 +0000

    prod. script

--- start_program.sh
+++ start_program.sh
@@ -1,3 +1,4 @@
 #command which will fired from dockerfile CMD ["./start_program.sh"]
+pkill -f python3
 cd  /home/odroid/projects/flask-video-streaming/
 source /opt/py3cv4/bin/activate.sh &&  /opt/py3cv4/bin/python3.5 pi_object_detection.py &
