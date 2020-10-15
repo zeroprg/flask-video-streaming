9d9442befeb50fce5697b0efcccb978cd1c75592
Vitaly Arkhipov <c-vitaly.arkhipov@charter.com>
2020-10-15 03:09:14 -0500

    fix

--- db.py
+++ db.py
@@ -88,8 +88,7 @@ def insert_frame(conn, hashcode, date, time, type, numpy_array, x_dim, y_dim, ca
        if cur.rowcount == 0:
           buffer = cv2.imencode('.jpg', numpy_array)[1]
           jpg_as_base64='data:image/jpeg;base64,'+ base64.b64encode(buffer).decode('utf-8')
-          cur.execute("INSERT INTO objects (hashcode, currentdate, currentime, type, frame, x_dim, y_dim, cam) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
-          (str(hashcode), date, time, type, str(jpg_as_base64), int(x_dim), int(y_dim), int(cam))
+          cur.execute("INSERT INTO objects (hashcode, currentdate, currentime, type, frame, x_dim, y_dim, cam) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",(str(hashcode), date, time, type, str(jpg_as_base64), int(x_dim), int(y_dim), int(cam))
     except Exception as e:
           print("type {} cam: {} e: {}".format( type, cam, e))
 
