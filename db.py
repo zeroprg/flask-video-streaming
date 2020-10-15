0be00ffaa8c4a1971b19c7bdebfb828d7691cb5b
Vitaly Arkhipov <c-vitaly.arkhipov@charter.com>
2020-10-15 02:04:52 -0500

    fixed insert frame

--- db.py
+++ db.py
@@ -89,11 +89,12 @@ def insert_frame(conn, hashcode, date, time, type, numpy_array, x_dim, y_dim, ca
     cur = conn.cursor()
     if y_dim == 0 or x_dim == 0 or  x_dim/y_dim > 5 or y_dim/x_dim > 5: return
     try:
-       cur.execute("UPDATE objects SET lastime=%s WHERE hashcode=%s", (time, str(hashcode)))
+       cur.execute("UPDATE objects SET currentime=%s WHERE hashcode=%s", (time, str(hashcode)))
        if cur.rowcount == 0:
           buffer = cv2.imencode('.jpg', numpy_array)[1]
           jpg_as_base64='data:image/jpeg;base64,'+ base64.b64encode(buffer).decode('utf-8')
-          cur.execute("INSERT INTO objects (hashcode, currentdate, currentime, type, frame, x_dim, y_dim, cam) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (str(hashcode), date, time, type, jpg_as_base64, x_dim, y_dim, cam))
+          cur.execute("INSERT INTO objects (hashcode, currentdate, currentime, type, frame, x_dim, y_dim, cam) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
+          (str(hashcode), date, time, type, jpg_as_base64, x_dim, y_dim, cam))
        conn.commit()
     except Error as e:
           print("type {} cam: {} e: {}".format( type, cam, e))
